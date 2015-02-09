69b8582767e49536d2f873f95f36deaac631a9f4
Josh Broton <joshbroton@gmail.com>
2015-02-09 14:36:13 -0600

    clean up gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -54,22 +54,3 @@ module.exports = function(grunt) {
         'watch'
     ]);
 };
-
-//for zip creation
-var dateHash = function() {
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth()+1; //January is 0!
-    var yyyy = today.getFullYear();
-
-    if(dd<10) {
-        dd='0'+dd
-    }
-
-    if(mm<10) {
-        mm='0'+mm
-    }
-
-    var date = yyyy + '-' + mm + '-' + dd + '.' + today.getTime();
-    return date;
-}
